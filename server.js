b5a2e6b454f06e72be408c1a71fdd344a80d79ce
Soham Sahare <44804981+sohamsahare123@users.noreply.github.com>
2020-08-06 21:34:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,54 +18,49 @@ io.on("connection", (socket) => {
     console.log("-----CLIENT CONNECTED: " + socket.id)
     connected_users.push(socket.id)
     // console.log(connected_users)
+  
+    socket.on("request_call", () => {
+        console.log("-----REQUESTING: "+ socket.id)
+        socket.broadcast.emit("request_call", socket.id)
+    })
 
-    if(connected_users.length <= 4){
-        socket.on("request_call", () => {
-            console.log("-----REQUESTING: "+ socket.id)
-            socket.broadcast.emit("request_call", socket.id)
-        })
+    socket.on("response_call", (id) => {
+        console.log(socket.id +" -----RESPONSING----- " + id) 
+        socket.to(id).emit("response_call", socket.id)
+    })
 
-        socket.on("response_call", (id) => {
-            console.log(socket.id +" -----RESPONSING----- " + id) 
-            socket.to(id).emit("response_call", socket.id)
-        })
-        
-        socket .on("offer", (id, offer) => {
-            console.log(socket.id +" -----OFFERING----- " + id) 
-            socket.to(id).emit("offer", socket.id, offer)
-        })
+    socket .on("offer", (id, offer) => {
+        console.log(socket.id +" -----OFFERING----- " + id) 
+        socket.to(id).emit("offer", socket.id, offer)
+    })
 
-        socket.on("answer", (id, answer) => {
-            console.log(socket.id +" -----ANSWERING----- " + id) 
-            socket.to(id).emit("answer", socket.id, answer)
-        })
+    socket.on("answer", (id, answer) => {
+        console.log(socket.id +" -----ANSWERING----- " + id) 
+        socket.to(id).emit("answer", socket.id, answer)
+    })
 
-        socket.on("ice", (id, ice) => {
-            console.log(socket.id +" -----ICING----- " + id) 
-            socket.to(id).emit("ice", socket.id, ice)
-        })
+    socket.on("ice", (id, ice) => {
+        console.log(socket.id +" -----ICING----- " + id) 
+        socket.to(id).emit("ice", socket.id, ice)
+    })
 
-        socket.on("disconnect", () => {
-            console.log("-----CLIENT DISCONNECTED: " + socket.id)
-            io.emit("delete", socket.id)
-            var index = connected_users.indexOf(socket.id)
-            if(index > -1){
-                connected_users.splice(index, 1)
-            }
-            // console.log(connected_users)
-        })
+    socket.on("disconnect", () => {
+        console.log("-----CLIENT DISCONNECTED: " + socket.id)
+        io.emit("delete", socket.id)
+        var index = connected_users.indexOf(socket.id)
+        if(index > -1){
+            connected_users.splice(index, 1)
+        }
+        // console.log(connected_users)
+    })
 
-        socket.on("mute", () => {
-            console.log("-----MUTING: "+ socket.id)
-            socket.broadcast.emit("mute", socket.id)
-        })
+    socket.on("mute", () => {
+        console.log("-----MUTING: "+ socket.id)
+        socket.broadcast.emit("mute", socket.id)
+    })
 
-        socket.on("unmute", () => {
-            console.log("-----UNMUTING: "+ socket.id)
-            socket.broadcast.emit("unmute", socket.id)
-        })
-    }
-    else{
-        socket.emit("full", socket.id)
-    }
+    socket.on("unmute", () => {
+        console.log("-----UNMUTING: "+ socket.id)
+        socket.broadcast.emit("unmute", socket.id)
+    })
 })
